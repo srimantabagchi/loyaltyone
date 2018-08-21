8ba0d5608d3c10a091e3c7c4ec2ea3cb35afd925
srimanta <srimantabagchi@gmail.com>
2018-08-21 00:22:26 +0000

    add REST webservice

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.get("/", function(req, res){
   res.send("Hello World");
+});
+
+app.get("/:text", function(req, res){
+  res.send("The text entered is: " + req.params.text);
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
